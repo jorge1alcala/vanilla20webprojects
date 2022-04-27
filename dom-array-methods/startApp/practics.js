b56a58bddd492a14021c54d49c03e4071704290f
Jorge Alcala <jorge1alcala@msn.com>
2022-04-27 12:40:49 -0700

    practics

--- dom-array-methods/startApp/practics.js
+++ dom-array-methods/startApp/practics.js
@@ -54,11 +54,24 @@ function updateDOM(providedData = data) {
     element.innerHTML = `<img class="picture" src="${user.picture}"><strong>${
       user.name
     }</strong> ${formatMoney(user.money)}`;
-    // event listener, check when each finish loading
-    // element.addEventListener('load', loading);
+
     main.appendChild(element);
   });
   loading();
+}
+// sort user by richest
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
+  updateDOM();
+}
+//filter only millionaires
+function showMillionaires() {
+  data = data.filter((user) => user.money > 1000000);
+  updateDOM();
+}
+
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
 }
 
 function formatMoney(num) {
@@ -67,3 +80,6 @@ function formatMoney(num) {
 
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMonay);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
